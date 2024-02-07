57ebb42dd89e5e27c2c88d46b2f1087959ea19f7
mattBava <XxpenguinxX123@gmail.com>
2024-02-07 13:26:57 -0500

    new changes

--- Disney app/Capstone 5/index.js
+++ Disney app/Capstone 5/index.js
@@ -2,123 +2,126 @@ import express, { application } from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 import pg from "pg";
+import env from "dotenv";
+import session from "express-session";
 
 const app = express();
 const port = 3000;
- 
+
 let bookSelect = 0;
 let titles = [];
 
-let reviews =[
-   
-];
-
+let reviews = [];
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "booknotes",
-    password: "Fu11St@ck",
-    port: 5432,
-  });
-  db.connect();
+  user: "postgres",
+  host: "localhost",
+  database: "booknotes",
+  password: "Fu11St@ck",
+  port: "5432",
+});
+db.connect();
 
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async(req,res) => {
-    reviews =[];
-    const result = await db.query("SELECT * FROM books ORDER BY id ASC");
-        reviews= result.rows;
-    res.render("main.ejs", {list:reviews});
-})
-
-app.get("/review", async(req,res) =>{
-    res.render("review.ejs", {url:null, check:false})
+app.get("/", async (req, res) => {
+  reviews = [];
+  const result = await db.query("SELECT * FROM books ORDER BY id ASC");
+  reviews = result.rows;
+  res.render("main.ejs", { list: reviews });
 });
 
-app.post("/review", async(req,res) =>{
-    const isbn = req.body.isbn;
-    titles =[];
-    if(isbn !== undefined){
-        const result = await axios.get("https://openlibrary.org/search.json?title="+isbn+"&fields=title,cover_i&limit=5");
-        for(var i = 0; i<5; i++){
-            if(result.data.docs[i] !== undefined){
-               titles.push(result.data.docs[i]); 
-            }
-        } 
-        res.render("review.ejs", {url:titles, check:true})
-    } 
+app.get("/review", async (req, res) => {
+  res.render("review.ejs", { url: null, check: false });
 });
 
-app.post("/select", async(req, res) => {
-    bookSelect = 0;
-    let postSelect;
+app.post("/review", async (req, res) => {
+  const isbn = req.body.isbn;
+  titles = [];
+  if (isbn !== undefined) {
+    const result = await axios.get(
+      "https://openlibrary.org/search.json?title=" +
+        isbn +
+        "&fields=title,cover_i&limit=5"
+    );
     for (var i = 0; i < 5; i++) {
-        postSelect = req.body[i];
-        if (!(typeof postSelect === 'undefined')) {
-           bookSelect = i;
-           console.log(bookSelect + titles[bookSelect].title)
-           res.render("review.ejs", {url:titles[bookSelect].cover_i, check:false});
-        }
+      if (result.data.docs[i] !== undefined) {
+        titles.push(result.data.docs[i]);
+      }
     }
-})
-
-app.post("/post", async(req,res) =>{
-let stars = 0;
-let postEdit;
-    for (var i = 0; i < titles.length; i++) {
-        postEdit = req.body[i];
-        console.log(postEdit)
-        if (!(typeof postEdit === 'undefined')) {
-           stars = i + 1;
-            const review = req.body.newPost;
-            console.log("working")
-           await db.query(
-            "INSERT INTO books (book,cover,review,starts) VALUES ($1,$2,$3,$4)",
-            [titles[bookSelect].title, titles[bookSelect].cover_i,review,stars]
-          );
-
-           res.redirect("/");
-        }
-    }
+    res.render("review.ejs", { url: titles, check: true });
+  }
 });
 
-app.post("/edit", async(req,res) =>{
-    bookSelect = 0;
-    let editSelect;
-    for (var i = 1; i <= reviews.length; i++) {
-        editSelect = req.body[i];
-        console.log(editSelect);
-        if (!(typeof editSelect === 'undefined')) {
-           bookSelect = i - 1;
-           res.render("edit.ejs", {edit:reviews, id:bookSelect});
-        }
+app.post("/select", async (req, res) => {
+  bookSelect = 0;
+  let postSelect;
+  for (var i = 0; i < 5; i++) {
+    postSelect = req.body[i];
+    if (!(typeof postSelect === "undefined")) {
+      bookSelect = i;
+      console.log(bookSelect + titles[bookSelect].title);
+      res.render("review.ejs", {
+        url: titles[bookSelect].cover_i,
+        check: false,
+      });
     }
+  }
 });
 
+app.post("/post", async (req, res) => {
+  let stars = 0;
+  let postEdit;
+  for (var i = 0; i < titles.length; i++) {
+    postEdit = req.body[i];
+    console.log(postEdit);
+    if (!(typeof postEdit === "undefined")) {
+      stars = i + 1;
+      const review = req.body.newPost;
+      console.log("working");
+      await db.query(
+        "INSERT INTO books (book,cover,review,starts) VALUES ($1,$2,$3,$4)",
+        [titles[bookSelect].title, titles[bookSelect].cover_i, review, stars]
+      );
 
-app.post("/update", async(req,res) =>{
-    let stars = 0;
-    let postEdit = null;
-        for (var i = 0; i <= 5; i++) {
-            postEdit = req.body[i];
-            console.log(postEdit)
-            if (!(typeof postEdit === 'undefined')) {
-               stars = i + 1;
-                const review = req.body.newPost;
-                console.log("updated")
-               await db.query(
-                "UPDATE books set review = ($1), starts = ($2) WHERE id = ($3)",
-                [review,stars,bookSelect+1]
-              );
-           res.redirect("/");
-            }
-        }
+      res.redirect("/");
+    }
+  }
 });
 
+app.post("/edit", async (req, res) => {
+  bookSelect = 0;
+  let editSelect;
+  for (var i = 1; i <= reviews.length; i++) {
+    editSelect = req.body[i];
+    console.log(editSelect);
+    if (!(typeof editSelect === "undefined")) {
+      bookSelect = i - 1;
+      res.render("edit.ejs", { edit: reviews, id: bookSelect });
+    }
+  }
+});
+
+app.post("/update", async (req, res) => {
+  let stars = 0;
+  let postEdit = null;
+  for (var i = 0; i <= 5; i++) {
+    postEdit = req.body[i];
+    console.log(postEdit);
+    if (!(typeof postEdit === "undefined")) {
+      stars = i + 1;
+      const review = req.body.newPost;
+      console.log("updated");
+      await db.query(
+        "UPDATE books set review = ($1), starts = ($2) WHERE id = ($3)",
+        [review, stars, bookSelect + 1]
+      );
+      res.redirect("/");
+    }
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-  
+  console.log(`Server running on port ${port}`);
+});
--- React portfolio page/my-portfolio/src/App.js
+++ React portfolio page/my-portfolio/src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./components/Header.jsx";
+import AboutMe from "./components/AboutMe.jsx";
+import Projects from "./components/Projects.jsx";
+import Contact from "./components/Contact.jsx";
 
 function App() {
   return (
-   <div class ="box">
-  
-
-   </div>
+    <div class="box">
+      <Header />
+      <AboutMe />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
